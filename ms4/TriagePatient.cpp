35eafcb20d2cbaa5918b0bb2e79ca537b251d64a
Wing Ho Chau <whchau1@myseneca.ca>
2024-03-29 17:27:28 -0400

    Minor change format

--- ms4/TriagePatient.cpp
+++ ms4/TriagePatient.cpp
@@ -11,6 +11,7 @@ I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
 -----------------------------------------------------------*/
 #define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
 #include "TriagePatient.h"
 
 namespace seneca {
